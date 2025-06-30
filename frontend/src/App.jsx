abff76ca69edfbb1ec5552fb02776c14d9816dcb
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-06-30 17:24:26 +0530

    feat : SellerLogin is completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,10 +13,11 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import AddAddress from './pages/AddAddress';
 import MyOrders from './pages/MyOrders';
+import SellerLogin from './components/seller/SellerLogin';
 
 function App() {
   const issellerpath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext();
+  const {showUserLogin,isSeller} = useAppContext();
   
   
    return (
@@ -40,7 +41,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}></Route>
         <Route path='/add-address' element={<AddAddress/>}></Route>
         <Route path='/my-orders' element={<MyOrders/>}></Route>
-
+        <Route path='/seller' element ={isSeller ? null : <SellerLogin/> } ></Route>
       </Routes>
    </div>
 
--- frontend/src/components/seller/SellerLogin.jsx
+++ frontend/src/components/seller/SellerLogin.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../../context/AppContext'
 
 const SellerLogin = () => {
-  return (
-    <div>SellerLogin</div>
+    const {isSeller, setIsSeller, navigate} = useAppContext();
+    const[email ,setEmail] = useState("");
+    const[password,setPassword] = useState("");
+
+    const onSubmitHandler = async(event)=>{
+        event.preventDefault();
+        setIsSeller(true);
+    }
+
+    useEffect(()=>{
+        if(isSeller){
+            navigate("/seller")
+        }
+    },[isSeller])
+  return !isSeller && (
+    <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+                <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shodow-xl border border-gray-200' > 
+                    <p className='text-2xl font-medium m-auto' ><span className='text-[#4fbf8b]'>Seller</span>Login</p>
+                     <div className='w-full'>
+                        <p>Email</p>
+                        <input onChange={(e) =>setEmail(e.target.value)} value={email} type="email"  placeholder='enter you email' className='border border-gray-200 rounded w-full p-2 mt-1 outline-[#4fbf8b]'
+                        required/>
+                    </div>
+                     <div  className='w-full'>
+                        <p>Password</p>
+                        <input onChange={(e) =>setPassword(e.target.value)} value = {password} type="password"  placeholder='enter your password' className='border border-gray-200 rounded w-full p-2 mt-1 outline-[#4fbf8b]' required/>
+                        
+                    </div>
+
+                    <button className='bg-[#4fbf8b] text-white w-full py-2 rounded-md cursor-pointer'> Login</button>
+                 </div>
+    </form>
   )
 }
 
-export default SellerLogin
+export default SellerLogin;
