ea99bc1017558dc3eb22787d0473c9b5f622189e
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-01 17:21:39 +0530

    seller routes are created for login, logout and isSellerAuth

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,6 +3,8 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './configs/db.js';
 import 'dotenv/config'
+import userRouter from './routes/userRoute.js';
+import sellerRouter from './routes/sellerRoute.js';
 
 const app = express();
 const port  = process.env.PORT || 4000;
@@ -21,9 +23,14 @@ app.use(cors({origin : allowedOrigins, Credentials: true}));
 
 app.get('/', (req,res) => res.send ("API is Working"));
 
+app.use('/api/user', userRouter);
+app.use('api/seller',sellerRouter);
+
 app.listen (port ,()=>{
     console.log(`Server is running on http://localhost:${port}`);
     
 })
+
+
 
 
