068855c81dc7493231d6d535b94ef5d1e03bf690
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-03 14:47:13 +0530

    prouctRoute

---
+++ Backend/routes/productProduct.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { upload } from "../configs/multer.js";
+import { isSellerAuth } from "../controllers/sellerController.js";
+import authSeller from "../middleware/authseller.js";
+import { addProduct, changeStoke, ProductById, productList } from "../controllers/productController.js";
+
+const productRouter = express.Router();
+
+productRouter.post('/add',upload.array(["images"]),authSeller,addProduct);
+productRouter.get('/list',productList);
+productRouter.get('/id',ProductById);
+productRouter.post('/id',authSeller,changeStoke);
+
+
+
+
+
+export default productRouter;
