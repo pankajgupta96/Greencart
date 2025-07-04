863c94318ccac7608efa18ee454a863cf9662890
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-04 23:12:13 +0530

    Update cloudnary.js

--- Backend/configs/cloudinay.js
+++ Backend/configs/cloudinay.js
@@ -1,13 +1,32 @@
-import {v2 as cloudinary} from "cloudinary"
+// import {v2 as cloudinary} from "cloudinary"
 
-const connectCloudinary = async () =>{
+// const connectCloudinary = async () =>{
+//     cloudinary.config({
+//         cloud_name :process.env.CLOUDINARY_CLOUD_NAME,
+//         api_key : process.env.CLOUDINARY_API_KEY,
+
+//         api_secret : process.env.CLOUDINARY_API_SECRET 
+//     })
+// }
+
+
+// export default connectCloudinary;
+
+
+import { v2 as cloudinary } from "cloudinary";
+
+const connectCloudinary = async () => {
+  try {
     cloudinary.config({
-        cloud_name :process.env.CLOUDINARY_CLOUD_NAME,
-        api_key : process.env.CLOUDINARY_API_KEY,
-
-        api_secret : process.env.CLOUDINARY_API_SECRET 
-    })
-}
-
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    console.log("Cloudinary connected!");
+  } catch (error) {
+    console.error("Cloudinary connection failed:", error);
+    process.exit(1);
+  }
+};
 
 export default connectCloudinary;
