d589483598805b05c45818ef1e4f5eb8699506e5
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-04 23:12:24 +0530

    Update multer.js

--- Backend/configs/multer.js
+++ Backend/configs/multer.js
@@ -1,3 +1,13 @@
+// import multer from 'multer';
+
+// export const upload = multer({storage: multer.diskStorage({})})
+
+
 import multer from 'multer';
 
-export const upload = multer({storage: multer.diskStorage({})})
+const upload = multer({
+  storage: multer.diskStorage({}),
+  limits: { fileSize: 5 * 1024 * 1024 } // 5MB limit
+});
+
+export { upload };
