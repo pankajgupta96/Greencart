d9c554b4864b41d1323911db2d7a308cebd806cd
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-04 23:14:20 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -24,13 +24,13 @@ const allowedOrigins = ['http://localhost:5173']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin : allowedOrigins, Credentials: true}));
+app.use(cors({origin : allowedOrigins, credentials: true}));
 
 
 app.get('/', (req,res) => res.send ("API is Working"));
 
 app.use('/api/user', userRouter);
-app.use('api/seller',sellerRouter);
+app.use('/api/seller',sellerRouter);
 app.use('/api/product',productRouter);
 app.use('/api/cart',cartRouter);
 app.use('/api/address',addressRouter);
