e140ae99aff963d926936f708039f2d701ed9b9b
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-05 23:30:45 +0530

    Update addressController

--- Backend/controllers/addressController.js
+++ Backend/controllers/addressController.js
@@ -5,7 +5,7 @@ import Address from "../models/Address.js";
 
 export const addAddress = async (req,res)=>{
     try {
-        const { address,userId} = req.body;
+        const {address,userId} = req.body;
         await Address.create({
             ...address,userId
         })
@@ -23,18 +23,28 @@ export const addAddress = async (req,res)=>{
 
   // get Address : api/address/get
 
-  export const getAddress = async (req,res)=>{
-    try {
+//   export const getAddress = async (req,res)=>{
+//     try {
      
-              const{ userId}  = req.body
-              const addresses = await Address.find({userId})
-              res.json({success : true , addresses})
-      } catch (error) {
-                console.log(error.message);
-        res.json({success:false , message : error.message});
+//               const{ userId}  = req.body
+//               const addresses = await Address.find({userId})
+//               res.json({success : true , addresses})
+//       } catch (error) {
+//                 console.log(error.message);
+//         res.json({success:false , message : error.message});
         
-    }
-  }
+//     }
+//   }
 
+
+export const getAddress = async (req, res) => {
+    try {
+        // userId comes from auth middleware
+        const addresses = await Address.find({ userId: req.userId });
+        res.json({ success: true, addresses });
+    } catch (error) {
+        res.json({ success: false, message: error.message });
+    }
+}
 
 
