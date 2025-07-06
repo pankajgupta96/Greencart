c4c5acd06265c28a0ca32a581659fb5d3ca390d6
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-06 23:18:45 +0530

    Create Loading.jsx

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,95 @@
+// // import React, { useEffect } from 'react';
+// // import { useAppContext } from '../context/AppContext';
+// // import { useLocation } from 'react-router-dom';
+
+// // const Loading = () => {
+
+// //     const {navigate} = useAppContext();
+// //     let {search} = useLocation();
+// //     const query = new URLSearchParams(search);
+// //     const nextUrl = query.get('next');
+
+// //     useEffect(()=>{
+// //         if( nextUrl){
+// //             setTimeout(()=>{
+// //                 navigate(`/${nextUrl}`);
+// //             },5000)
+// //         }
+// //     },[nextUrl]);
+
+// //    return (
+// //     <div className='flex justify-center items-center h-screen'>
+// //       <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-[#4fbf8b]'></div>
+// //     </div>
+// //   );
+// // };
+
+// // export default Loading;
+
+
+
+
+// import React, { useEffect } from 'react'
+// import { useAppContext } from '../context/AppContext'
+// import { useLocation } from 'react-router-dom'
+
+// const Loading = () => {
+//     const {navigate} = useAppContext()
+//     let {search} = useLocation()
+//     const query = new URLSearchParams(search)
+//     const nextUrl = query.get('next')
+
+//     useEffect(() => {
+//         if(nextUrl) {
+//            setTimeout(()=>{
+//                navigate(`${nextUrl}  `)
+            
+//            },5000)
+//         } else {
+//             navigate('/')
+//         }   
+//     },[nextUrl])
+//   return (
+//     <div className='flex justify-center items-center h-screen'>
+//         <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-[#4fbf8b]'>
+
+
+//         </div>
+
+//     </div>
+//   )
+// }
+
+// export default 
+
+
+
+
+import React, { useEffect } from 'react'
+import { useAppContext } from '../context/AppContext'
+import { useLocation } from 'react-router-dom'
+
+const Loading = () => {
+    const { navigate } = useAppContext()
+    let { search } = useLocation()
+    const query = new URLSearchParams(search)
+    const nextUrl = query.get('next')
+
+    useEffect(() => {
+        if (nextUrl) {
+            setTimeout(() => {
+                navigate(nextUrl.trim())  // ✅ Trimmed
+            }, 5000)
+        } else {
+            navigate('/')
+        }
+    }, [nextUrl])
+
+    return (
+        <div className='flex justify-center items-center h-screen'>
+            <div className='animate-spin rounded-full h-24 w-24 border-4 border-gray-300 border-t-[#4fbf8b]'></div>
+        </div>
+    )
+}
+
+export default Loading;
