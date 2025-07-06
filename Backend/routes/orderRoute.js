0a6371312a1030aa4f9ed15f995bc5996a29c434
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-06 23:17:45 +0530

    Update orderRoute.js

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -1,14 +1,16 @@
 import express from 'express'
 import authUser from '../middleware/authUser.js';
-import { getAllOrders, getUserOrders, placeOrderCOD } from '../controllers/orderController.js';
+import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe } from '../controllers/orderController.js';
 import authSeller from '../middleware/authseller.js';
+import Stripe from 'stripe';
 
 const orderRouter = express.Router();
 
 
 
 orderRouter.post('/cod' ,authUser ,placeOrderCOD);
-orderRouter.get('/cod/user' ,authUser ,getUserOrders);
+orderRouter.post('/Stripe' ,authUser ,placeOrderStripe);
+orderRouter.get('/user' ,authUser ,getUserOrders);
 orderRouter.get('/seller' ,authSeller , getAllOrders);
 
 export default orderRouter;
