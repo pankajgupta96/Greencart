8f82f083dba97dbc30a5d498ba28d0d08218a0e5
Pankaj Gupta <pankajgupta3030468@gmail.com>
2025-07-07 12:08:39 +0530

    Update modelOrder.js

--- Backend/models/Order.js
+++ Backend/models/Order.js
@@ -1,27 +1,21 @@
 import mongoose from "mongoose";
 
-const orderSchema  =  new mongoose.Schema({
-      userId : {type:String , required : true , ref :'user'},
-      items : [{
-        product : {type: String , required : true , ref: 'product'},
-        quantity: {type : Number , required: true }
-      }
-    
-    ] ,
+const orderSchema = new mongoose.Schema({
+  userId : {type :String, required : true , ref: "user"},
+  items:[{
+    product :{type :String, required : true, ref: "product"},
+    quantity : {type : Number, required : true},
+   
+  }],
+  amount: {type : Number, required : true},
+  address:{type: String, required : true ,ref: "address"  },
+  status:{type: String, default: "Order Placed"},
+  paymentType:{type: String, required : true},
+   isPaid:{type: Boolean, required : true, default: false},
 
-     amount : {type : Number , required : true},
-     address : {type : String , required : true , ref : 'address'},
-     status : {type : String , default : 'Order placed'},
-     paymentType : {type : String , required : true},
-     ispaid : {type : Boolean , required:true},
-
-
+},{timestamps: true});
 
 
+const Order = mongoose.models.order || mongoose.model("order", orderSchema);
 
-},{ timestamps : true})
-
-
-const order = mongoose.model.order || mongoose.model('order',orderSchema)
-
-export default order;
+export default Order;
